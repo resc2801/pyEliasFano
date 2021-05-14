5f436f4e5103d290c047b0c344d395698c308188
rmrschub <rene.schubotz@dfki.de>
2021-05-14 11:56:47 +0200

    fixing pypi markdown issue

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 from setuptools import setup
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+    long_description_content_type = "text/x-rst"
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+    long_description_content_type = 'text/markdown'
+
 setup(
     name='pyEliasFano',
     version='0.0.1',
     description='pyEliasFano offers a **quasi-succinct** represenation for a monotone non-decreasing sequence of n integers from the universe [0 . . . m) occupying 2*n+n*ceil(log2(m/n)) bits.',
+    long_description=long_description,
+    long_description_content_type=long_description_content_type,
     url='https://github.com/rmrschub/pyEliasFano',
     author='René Schubotz',
     author_email='rene.schubotz@dfki.de',
     license='CC BY-NC-SA 4.0',
     packages=['pyEliasFano'],
-    install_requires=['math',
-                      'typing',
+    install_requires=['typing',
                       'bitarray',
                       'unary_coding',
                       ],
